5e9538650b6b902ad7e7e995203d63de0b6f47d5
HP <HP@HP-PC.gateway.2wire.net>
2015-07-09 14:37:14 -0400

    IMPLEMENTED THE POST METHOD

--- src/java/com/Assignment4/Products.java
+++ src/java/com/Assignment4/Products.java
@@ -10,22 +10,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.json.JsonObject;
+import javax.persistence.Id;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.simple.parser.JSONParser;
 
+import org.json.simple.parser.ParseException;
 /**
  * REST Web Service
  *
  * @author HP
  */
-@Path("product")
+@Path("/")
 public class Products {
    Products items = new Products();
     ArrayList<Products> product = new ArrayList<>();
@@ -63,7 +69,8 @@ public class Products {
            Products items = new Products(result.getInt("ProductId"), result.getString("name"), result.getString("description"), result.getInt("quantity"));
            product.add(items);
        }
-         return product;
+        
+       return product;
        }
     
     @GET
@@ -73,18 +80,44 @@ public class Products {
     {
         Connection conn = getConnection();
         Statement s = conn.createStatement();
-        ResultSet rs = s.executeQuery("select * from product where productid="+id);
+        ResultSet rs = s.executeQuery("select * from product where ProductID="+id);
 
       
         while (rs.next()) {
 
-            Products pro = new Products(rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity"));
+            Products pro = new Products(rs.getInt("ProductID"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity"));
             product.add(pro);
         }
 
         return product;
     }
+   @POST
+   @Path("/product")
+   @Consumes(MediaType.APPLICATION_JSON)
+   public void createProducts(String str) throws ParseException, SQLException{
+     
+       JSONParser jparser = new JSONParser();
+       JsonObject jobject = (JsonObject) jparser.parse(str);
 
+       Object ID = jobject.get("id");
+       String ProductID = ID.toString();
+       int iD = Integer.parseInt(ProductID);
+       
+       Object Name = jobject.get("name");
+       String name = Name.toString();
+       
+       Object Description = jobject.get("description");
+       String description = Description.toString();
+       
+       Object Qty = jobject.get("quantity");
+       String quantity = Qty.toString();
+       int Quantity = Integer.parseInt(quantity);
+       
+       Connection conn = getConnection();
+        Statement s = conn.createStatement();
+        s.executeUpdate("INSERT INTO product VALUES ('"+iD+"','"+name+"','"+description+"','"+quantity+"' )");
+   }
+    
     /**
      * PUT method for updating or creating an instance of Products
      * @param content representation for the resource
